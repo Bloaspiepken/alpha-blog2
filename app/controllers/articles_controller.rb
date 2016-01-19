ca8165840bd6657069d0f42b8bce799d807bce8e
Bloaspiepken <philip.nijhof@icloud.com>
2016-01-19 20:24:22 +0000

    Searching my error undefined mthod updated_at

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   
   
   def create
-    debugger
+    #debugger
     @article = Article.new(article_params)
     @article.user = User.first
   
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   def update
     if @article.update(article_params)
     
-      flash[:success] = "Article was succesfully updated"
+      flash[:succes] = "Article was succesfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
