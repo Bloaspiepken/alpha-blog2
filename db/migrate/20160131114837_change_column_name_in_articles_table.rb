f59dee5a382130fed2aa8172a907ae08872a336e
Bloaspiepken <philip.nijhof@icloud.com>
2016-01-31 13:04:17 +0000

    change updated in updated_at

--- db/migrate/20160131114837_change_column_name_in_articles_table.rb
+++ db/migrate/20160131114837_change_column_name_in_articles_table.rb
@@ -1,5 +1,5 @@
 class ChangeColumnNameInArticlesTable < ActiveRecord::Migration
   def change
-    rename_column :articles, :update, :updated_at
+    rename_column :articles, :updated, :updated_at
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126213137) do
+ActiveRecord::Schema.define(version: 20160131114837) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
